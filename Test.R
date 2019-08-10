ccef3507eccf0d958800c1b993c278fb0866ca9c
aletvandeventer35 <53835175+aletvandeventer35@users.noreply.github.com>
2019-08-10 21:17:56 +0200

    Add files via upload

---
+++ Test.R
@@ -0,0 +1,20 @@
+---
+title: "test-report"
+author: "Alet van Deventer"
+output: pdf_document
+---
+  
+```{r eval=FALSE)
+a <- 2
+```
+
+```{r include=FALSE}
+print("Hello World!")
+a <- 5
+```
+
+```{r echo=FALSE}
+a <- a+1
+print(a)
+```
+
